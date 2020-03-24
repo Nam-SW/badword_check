8b446ade5f4406ac714d39ec11436060a95f2af6
Nam-Seung-Woo <45066060+Nam-SW@users.noreply.github.com>
2020-03-25 00:25:44 +0900

    데이터 정리 및 시각화

---
+++ show_graph.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from text import encode
+import matplotlib.pyplot as plt
+
+bad = pd.read_excel('./dataset/badword.xlsx', encoding='utf-8-sig')
+good = pd.read_excel('./dataset/goodword.xlsx', encoding='utf-8-sig')
+
+print(len(bad), len(good))
+
+df = pd.concat([bad, good])
+df.dropna(how='any', inplace=True)
+print(len(df))
+
+
+for i in range(len(df)):
+    d = df.iloc[i]
+    df.iloc[i] = [encode(d[0]) , d[1]]
+
+df['length'] = [len(df.iloc[i][0]) for i in range(len(df))]
+print(df.head())
+
+plt.hist(df['length'], bins=df['length'].max())
+plt.show()
