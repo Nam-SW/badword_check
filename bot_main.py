9e9d8e3730cc379a604346cb430d918dc306a2e5
Nam-Seung-Woo <45066060+Nam-SW@users.noreply.github.com>
2020-09-15 10:25:04 +0900

    Remove: repository 전면 개편을 위한 삭제

--- bot_main.py
+++
@@ -1,5 +0,0 @@
-from discordBot.dataBot import Bot_on
-
-
-if __name__ == "__main__":
-    Bot_on()
--- data_main.py
+++
@@ -1,6 +0,0 @@
-from AI import AI
-
-
-if __name__ == "__main__":
-    AI.check_data_count()
-    AI.show_graph()
